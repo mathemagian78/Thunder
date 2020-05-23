163af6410b8320a7d9932140ee1f1a5fdeed8b3a
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-05-23 12:52:14 -0700

    Updates

---
+++ dev/precache-manifest.3a4982dc8ea0fdba86ba696628fadadf.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae686f6887010046d27f562ba3773e2",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/Celery.png"
+  },
+  {
+    "revision": "1263cea2be926ca240630a28dc45fa19",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/Poof.png"
+  },
+  {
+    "revision": "0fb6f93a0fc9e88b4e3ba420107c87ea",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/Room.png"
+  },
+  {
+    "revision": "9ed6cb8feb446c130e2cb805065b5b7a",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/Skull.png"
+  },
+  {
+    "revision": "9a2588a1a1a339966b3675117ecc0913",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/Thunder.png"
+  },
+  {
+    "revision": "bd2bfd2402a6df2a9747ca86ba4b5a87",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/Thunder2.png"
+  },
+  {
+    "revision": "90b7be82cd37eb98b01863f50b84ab62",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/bunny-hop-spritesheet.png"
+  },
+  {
+    "revision": "68915bb265ad4941b284d5b28494dc3d",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/carrot.png"
+  },
+  {
+    "revision": "c9dd2677dfd4eba58e3ed0efd8ef4795",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/carrotTime.png"
+  },
+  {
+    "revision": "004817e878fdab981a83b7e3c0ad0939",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/citations.md"
+  },
+  {
+    "revision": "0ac26c5a70b28d696adba52a4074c533",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/environment_forest_alt1.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/font.xml"
+  },
+  {
+    "revision": "8822c0c57b608c01898755ea857b4952",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/fox-SWEN-bright.png"
+  },
+  {
+    "revision": "5c5eb626426eb420f3e5f4ca68c4cfb0",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/grass.png"
+  },
+  {
+    "revision": "1091ad6d4686178a215475256cd015ec",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/music.mp3"
+  },
+  {
+    "revision": "8c17c1515edf3260716c7812c638e3cb",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/pa2.png"
+  },
+  {
+    "revision": "e0d390c783f610d638e5c34e8005772c",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/pleasing-bell.wav"
+  },
+  {
+    "revision": "cae8bdd56df6425d46278ab33a748ee9",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/pop.ogg"
+  },
+  {
+    "revision": "4784118f47d91136d4cba54fee347994",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/rabbit.png"
+  },
+  {
+    "revision": "e7564671b32f498a76f09c4ff3814906",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/roomButton.png"
+  },
+  {
+    "revision": "3a9c85fe5ad487a578aa67a6098fb6ac",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/screenshot_1.png"
+  },
+  {
+    "revision": "1286ed7b814c70e7844f2ac01f1a3a78",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/wave.wav"
+  },
+  {
+    "revision": "592f044bf13557d9f9aa6ec9e1beed83",
+    "url": "/coding-2-try-phaser-Mathemagician78/devassets/win.wav"
+  },
+  {
+    "revision": "7bb496768fc5b4e47357",
+    "url": "/coding-2-try-phaser-Mathemagician78/devgen.main.98afc347c485a38f51d9.bundle.js"
+  },
+  {
+    "revision": "b2512ad1dc1074ce2bf5",
+    "url": "/coding-2-try-phaser-Mathemagician78/devgen.vendors.ed464eb967a0780a06d8.bundle.js"
+  },
+  {
+    "revision": "7327dec26977508dbe111fe74586ed44",
+    "url": "/coding-2-try-phaser-Mathemagician78/devindex.html"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "/coding-2-try-phaser-Mathemagician78/devmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/coding-2-try-phaser-Mathemagician78/devsw.js"
+  }
+]);
---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/coding-2-try-phaser-Mathemagician78/devprecache-manifest.3a4982dc8ea0fdba86ba696628fadadf.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
